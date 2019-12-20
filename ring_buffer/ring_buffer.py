6d4a3bce86247cca0e6387eb053405785cf30eca
laura <lauradondiego@gmail.com>
2019-12-20 12:00:54 -0500

    passed 1 failed 1 on ring_buffer not yet complete

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -4,29 +4,38 @@ from doubly_linked_list import DoublyLinkedList
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.current = None
+        self.current = 0
         self.storage = DoublyLinkedList()
 
-    def append(self, item):
-        pass
+    def append(self, item):  # adds elements to the buffer
+        # when full, the oldest element is overwritten with the newest
+        # fixed size
+        # append element at the end of the buffer
+        # data append in head (newest), data pop from tail (oldest)
+        if self.current >= self.capacity:
+            self.storage.remove_from_tail(
+                self.current)  # remove the oldest item
+            self.storage.add_to_head(self.current, item)
 
-    def get(self):
+    def get(self):  # returns all elements in an ordered list
+        # except for NONE
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
 
-        return list_buffer_contents
+        if list_buffer_contents is not None:
+            return list_buffer_contents
 
 # ----------------Stretch Goal-------------------
 
 
-class ArrayRingBuffer:
-    def __init__(self, capacity):
-        pass
+# class ArrayRingBuffer:
+#     def __init__(self, capacity):
+#         pass
 
-    def append(self, item):
-        pass
+#     def append(self, item):
+#         pass
 
-    def get(self):
-        pass
+#     def get(self):
+#         pass
